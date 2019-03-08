b1c593fe598edfcc865a7f87e5ed25120e4a0c62
Marven <240902545@qq.com>
2019-03-08 17:48:38 +0800

    update build

--- app/src/main/java/com/phsxy/library/MainActivity.java
+++ app/src/main/java/com/phsxy/library/MainActivity.java
@@ -3,12 +3,14 @@ package com.phsxy.library;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.phsxy.utils.BankInfoUtil;
+import com.phsxy.utils.StringUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
