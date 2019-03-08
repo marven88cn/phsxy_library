2ad89e7887b478807c4f52a5bd022fc875bade2c
Marven <240902545@qq.com>
2019-03-08 19:36:22 +0800

    update build2

---
+++ utils/src/main/java/com/phsxy/utils/FileUtils.java
@@ -0,0 +1,11 @@
+package com.phsxy.utils;
+
+/**
+ * Description: 文件工具类
+ * Copyright  : Copyright (c) 2018
+ * Company    : 普华
+ * Author     : marven
+ * Date       : 2019/3/8 19:29
+ */
+public class FileUtils {
+}
