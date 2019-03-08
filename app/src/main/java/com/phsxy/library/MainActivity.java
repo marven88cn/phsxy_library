930d6c43d581d885d22d5d6ee84ab8156914190e
Marven <240902545@qq.com>
2019-03-08 20:57:03 +0800

    delete resoure

--- app/src/main/java/com/phsxy/library/MainActivity.java
+++ app/src/main/java/com/phsxy/library/MainActivity.java
@@ -3,8 +3,6 @@ package com.phsxy.library;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.phsxy.utils.BankInfoUtil;
-import com.phsxy.utils.StringUtils;
 
 public class MainActivity extends AppCompatActivity {
 
